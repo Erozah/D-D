24d39809b4fdfadded3a4f534f624e5674d5b5da
Erozah <mickael.plantard@gmail.com>
2026-07-17 16:33:37 +0200

    Added empty classes

---
+++ DD/src/Main.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+final class Main {
+	Main() {
+	}
+
+	void main() {
+		IO.println(String.format("Hello and welcome!"));
+
+		for(int i = 1; i <= 5; ++i) {
+			System.out.println("i = " + i);
+		}
+
+	}
+}
